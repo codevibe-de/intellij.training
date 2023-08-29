202bb8b1fb00f6accff989d8bc0aeb0cd2fa2de3
Thomas Auinger <thomas@auinger.de>
2023-08-29 19:55:12 +0200

    testing incl run-config

---
+++ testing/src/main/java/de/codevibe/IdGenerator.java
@@ -0,0 +1,23 @@
+package de.codevibe;
+
+import java.util.Random;
+
+public class IdGenerator {
+
+    private int length;
+
+    private String prefix;
+
+    private String suffix;
+
+    public IdGenerator(int length, String prefix, String suffix) {
+        this.length = length;
+        this.prefix = prefix;
+        this.suffix = suffix;
+    }
+
+    public String generateId() {
+        var nstr = String.format("%d", new Random().nextInt()); // fixme
+        return prefix + nstr + suffix;
+    }
+}
---
+++ testing/src/test/java/de/codevibe/IdGeneratorTest.java
@@ -0,0 +1,25 @@
+package de.codevibe;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class IdGeneratorTest {
+
+    private IdGenerator generator;
+
+    @BeforeEach
+    void setUp() {
+        this.generator = new IdGenerator(3, "abc", "xyz");
+    }
+
+    @Test
+    void generateId() {
+        // when
+        String id = this.generator.generateId();
+
+        // then
+        Assertions.assertThat(id).hasSize(9);
+        // todo add checks for prefix & suffix
+    }
+}
