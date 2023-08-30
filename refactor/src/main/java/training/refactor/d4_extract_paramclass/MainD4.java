1297c79a5e9a1cf96ee547ca88a3b466360685d2
Thomas Auinger <thomas@auinger.de>
2023-08-30 12:03:39 +0200

    added refactor.MainD4

---
+++ refactor/src/main/java/training/refactor/d4_extract_paramclass/MainD4.java
@@ -0,0 +1,8 @@
+package training.refactor.d4_extract_paramclass;
+
+public class MainD4 {
+
+    public static void main(String[] args) {
+        new ComplexService().calculate(1, 2, 3.2d, 0.1d);
+    }
+}
