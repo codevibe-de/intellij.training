97607dfc79cc76a58ec2f478beb463097c548bad
Thomas Auinger <thomas@auinger.de>
2023-09-27 12:10:12 +0200

    improved todos, added hints

--- refactor/src/main/java/training/refactor/d3_extract_superclass/DefaultProductRepository.java
+++ refactor/src/main/java/training/refactor/d3_extract_superclass/DefaultProductRepository.java
@@ -4,8 +4,11 @@ import training.refactor.Product;
 
 import java.util.ArrayList;
 
-// TODO extract a superclass `AbstractRepository` which contains a list of generic items
+// TODO extract a superclass `AbstractRepository`, which can manage a list of generic items
+// Hint: "Change Signature" might be useful for the new abstract class...
+
 // TODO rename fields and methods as appropriate
+// Hint: rename the field first
 public class DefaultProductRepository {
 
     private ArrayList<Product> products = new ArrayList<>();
