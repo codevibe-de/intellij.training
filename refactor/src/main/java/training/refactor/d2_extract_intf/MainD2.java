e7bcf06d0a298ccacc037bba61dabd9dbc98cd7d
Thomas Auinger <thomas@auinger.de>
2023-09-27 12:02:25 +0200

    added MainD2

---
+++ refactor/src/main/java/training/refactor/d2_extract_intf/MainD2.java
@@ -0,0 +1,8 @@
+package training.refactor.d2_extract_intf;
+
+public class MainD2 {
+
+    public static void main(String[] args) {
+        DefaultProductRepository repo = new DefaultProductRepository();
+    }
+}
